c04740d4cee6595afa2fc85bf1b27a36337f0e95
Natalie <natalie@huridocs.org>
2019-11-11 09:12:41 +0100

    bugfix random state

--- sample.py
+++ sample.py
@@ -6,11 +6,12 @@ def sample_data(data, labels, MAX_N, random_state=None):
     for ind, label in enumerate(labels):
         category_data = data[data.str_label.str.contains(label)]
         if len(category_data) < MAX_N:
-            samples.append(category_data.sample(len(category_data)),
-                           random_state=random_state)
+            category_sample = category_data.sample(len(category_data),
+                                                   random_state=random_state)
+            samples.append(category_sample)
         else:
-            samples.append(category_data.sample(MAX_N),
-                           random_state=random_state)
+            samples.append(category_data.sample(MAX_N,
+                                                random_state=random_state))
     samples_df = pd.concat(samples)
     samples_df.drop_duplicates(subset=['text', 'str_label'], inplace=True)
     return samples_df
