dc2319f878b157610d0ba28b68b8ec2b6482e468
Natalie <natalie@huridocs.org>
2019-11-12 12:27:47 +0100

    update evaluation function

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -13,22 +13,24 @@ def compute_metrics(confusion_matrices, categories):
     evaluation = {}
     for ind, matrix in enumerate(confusion_matrices):
         evaluation[categories[ind]] = metrics_from_confusion_matrix(matrix)
-    evaluation_df = pd.DataFrame.from_dict(evaluation).transpose()
-    evaluation_df.sort_values(['f1', 'prec', 'recall'], ascending=False, inplace=True)
-    evaluation_df.loc['mean'] = evaluation_df.mean(axis=0)
-    return evaluation_df
+    return pd.DataFrame.from_dict(evaluation).transpose()
 
 
 def metrics_from_confusion_matrix(matrix):
     tn, fp, fn, tp = matrix.ravel()
     precision, recall, f1 = prec_rec_fscore(tp, fp, fn)
-    return {'f1': round(f1, 4), 'prec': round(precision, 4), 'recall': round(recall, 4)}
+    return {'f1': round(f1, 4), 'prec': round(precision, 4), 'recall':
+            round(recall, 4), 'tp': tp, 'fp': fp, 'fn': fn}
 
 
 def evaluate(data, labels):
     confusion_matrices = confusion_matrix(data)
     evaluation = compute_metrics(confusion_matrices, labels)
-    return evaluation
+    micro_avg = micro_average(evaluation)
+    evaluation = evaluation.append(pd.DataFrame(micro_avg, index=['micro_avg']))
+    evaluation.loc['macro_avg'] = evaluation.mean(axis=0)
+    return evaluation.reindex(columns=['f1', 'prec', 'recall', 'tp', 'fp', 'fn'])
+
 
 def f_score(precision, recall, b=1):
     prec = np.array(precision)
