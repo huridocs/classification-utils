b151d0d6d8238af0c91c39686c15639d2ea24ca2
Benjamin Dittes <bdittes@google.com>
2020-01-07 17:19:40 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os.path
+import sys
+
+from setuptools import find_packages, setup
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+sys.path.insert(0, this_directory)
+
+setup(
+    name='huridocs-classification-utils',
+    version=0.1,
+    description='Classifications utils',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/huridocs/classification-utils',
+    author='Huridocs',
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={},
+    python_requires='>=3.6',
+    entry_points={},
+)
